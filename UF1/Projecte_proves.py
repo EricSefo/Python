1e36e028ef4bbf0191fad1a69c3cca6b458dd886
EricSefo <nasim2003essousi@gmail.com>
2022-11-10 00:07:45 +0100

    dfgfd

--- UF1/Projecte_proves.py
+++ UF1/Projecte_proves.py
@@ -1,17 +1,24 @@
 import random
 
-options = [ ["Pregunta 1: Dis-me l'arrel de quadrada de 4.",["1. 1.5\n","2. 2\n","3. 5"],2],
-            ["Pregunta 2: Dis-me l'arrel de quadrada de 16.",["1. 4\n","2. 2\n","3. 6"],1],
-            ["Pregunta 3: Quin és el record dels 100 metros llisos en segons?",["1. 9,58\n","2. 10\n","3. 9,78"],1],
-            ["Pregunta 4: Quants milions de persones viuen a españa?","1. 47\n2. 50\n3. 55",1],
-            ["Pregunta 5: En quin segle estem?","1. 22\n2. 20\n3. 21",3],
-            ["Pregunta 6: Quin nivell d'anglès és el més alt?","1. C2\n2. C1\n3. B2",1],
-            ["Pregunta 7: Quin edifici és el més alt del món?","1. Burj Khalifa\n2. Torre de Shanghái\n3. One World Trade Center",1],
-            ["Pregunta 8: En quin any va acabar la segona guerra mundial?","1. 1934\n2. 1945\n3. 1915",2],
-            ["Pregunta 9: Quin és millor mestre d'ASIX?","1. Mireia\n2. Jordi Varas\n3. Gonçal",3],
-            ["Pregunta 10: Dis-me l'arrel quadrada de 25.","1. 7\n2. 5\n3. 6",2] ]
+options = [ ["Pregunta 1: Dis-me l'arrel de quadrada de 4.",["1. 1.5\n","2. 2\n","3. 5\n"],2],
+            ["Pregunta 2: Dis-me l'arrel de quadrada de 16.",["1. 4\n","2. 2\n","3. 6\n"],1],
+            ["Pregunta 3: Quin és el record dels 100 metros llisos en segons?",["1. 9,58\n","2. 10\n","3. 9,78\n"],1],
+            ["Pregunta 4: Quants milions de persones viuen a españa?",["1. 47\n","2. 50\n","3. 55\n"],1],
+            ["Pregunta 5: En quin segle estem?",["1. 22\n","2. 20\n","3. 21\n"],3],
+            ["Pregunta 6: Quin nivell d'anglès és el més alt?",["1. C2\n","2. C1\n","3. B2\n"],1],
+            ["Pregunta 7: Quin edifici és el més alt del món?",["1. Burj Khalifa\n","2. Torre de Shanghái\n","3. One World Trade Center\n"],1],
+            ["Pregunta 8: En quin any va acabar la segona guerra mundial?",["1. 1934\n","2. 1945\n","3. 1915\n"],2],
+            ["Pregunta 9: Quin és millor mestre d'ASIX?",["1. Mireia\n","2. Jordi Varas\n","3. Gonçal\n"],3],
+            ["Pregunta 10: Dis-me l'arrel quadrada de 25.",["1. 7\n","2. 5\n","3. 6\n"],2] ]
+
 aleatori = random.choice(options)
-print (aleatori[1][0])
+response = options.index(aleatori)
+random.shuffle(aleatori[1])
+for i in range(len(pregunta)):
+    print(str(i+1)+". "+aleatori[i][0])
+    
+print (aleatori[0]+"\n",StrA,StrB,StrC)
+
 victoria_j1 = 0
 victoria_j2 = 0
 victoria_j3 = 0
@@ -37,8 +44,12 @@ while True:
     elif len(options) != 0:
         aleatori = random.choice(options)
         response = options.index(aleatori)
-        print (aleatori[0]+"\n"+aleatori[1])
-        if torn == 1:
+        random.shuffle(aleatori[1])
+        StrA = "".join(aleatori[1][0])
+        StrB = "".join(aleatori[1][1])
+        StrC = "".join(aleatori[1][2])
+        print (aleatori[0]+"\n"+StrA+StrB+StrC)
+        if torn == 1: 
             answer = int(input("Jugador 1 (RESPOSTA): "))
             if answer == aleatori[2]:
                 victoria_j1 += 1
@@ -74,20 +85,3 @@ while True:
     else:
         print("No hi ha més preguntes, el joc s'acabat")
         break'''
-
-'''#Dos jugadors, el primer que encerta 3 preguntes guanya!!'''
-'''#El format de les preguntes serà:
-#Enunciat pregunta:
-#Resposta 1
-#Resposta 2
-#Resposta 3'''
-'''#S'ha de mostrar un marcador de preguntes encertades per cada jugador.'''
-'''# Cada jugador anirà responent preguntes fins que falli.'''
-'''#El torn d'inici serà aleatori.'''
-'''#Si S'han realitzat 10 preguntes i no hi ha guanyador el joc acaba.'''
-'''#Les preguntes es mostraran en ordre aleatori.'''
-'''#Heu de controlar quines preguntes ja s'han plantejat als jugadors.'''
-
-#OPCIONAL:
-#Les respostes es mostraran en ordre aleatori.
-'''#Podem escollir el nombre de jugadors entre 2 i 4'''
