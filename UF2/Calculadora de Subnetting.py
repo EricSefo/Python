5db166587d479d713eb0ed3a4242553f48495394
EricSefo <nasim2003essousi@gmail.com>
2023-04-06 23:01:25 +0200

    A

--- UF2/Calculadora de Subnetting.py
+++ UF2/Calculadora de Subnetting.py
@@ -18,50 +18,75 @@ def decimal(x):
         q += int(x[-1])*(2**z)
         z += 1
         return decimal(x[:-1])
+    
+def conversio(p,w,x=None):
+    global z
+    global q
+    global y
+    if w == decimal:
+        for i in x.split("."):
+            z = int()
+            q = int()
+            p += str(w(i))
+            if p.count(".") < 3:
+                p += "."
+        return p
+    elif w == binari:
+        for i in list(map(int ,input("IP: ").split("."))):
+            y = str()
+            p += w(i)
+            if p.count(".") < 3:
+                p += "."
+        return p
+    
+def autocompletar(x):
+    while (x.count("1") + x.count("0")) != 32:
+        if (x.count("1") + x.count("0")) % 8 == 0:
+            x += '.'
+        x += '0'
+    return x
 
 a = str()
 b = str()
-for i in list(map(int ,input("IP: ").split("."))):
-    y = str()
-    a += binari(i)+"."
-for i in a[:-1].split("."):
-    z = int()
-    q = int()
-    b += str(decimal(i))+"."
+a = conversio(a,binari)
+b = conversio(b,decimal,a)
+
 masc = int(input("Màscara de xarxa: "))
 if masc >= 0 and masc <= 32:
-    binari = str()
+    masc_binari = str()
     for i in range (masc):
-        binari += '1'
-        if binari.count("1") % 8 == 0:
-            binari += '.'
-    while (binari.count("1") + binari.count("0")) != 32:
-        if (binari.count("1") + binari.count("0")) % 8 == 0:
-            binari += '.'
-        binari += '0'
+        masc_binari += '1'
+        if masc_binari.count("1") % 8 == 0:
+            masc_binari += '.'
+    masc_binari = autocompletar(masc_binari)
 else:
     print("Màscara incorrecta")
+    exit()
 
 decimal_masc = str()
-for i in binari.split("."):
-    z = int()
-    q = int()
-    decimal_masc += str(decimal(i))+"."
+decimal_masc = conversio(decimal_masc,decimal,masc_binari)
 
 if 0 <= int(b.split(".")[0]) <= 127 and 0 <= int(b.split(".")[1]) <= 255 and 0 <= int(b.split(".")[2]) <= 255 and 0 <= int(b.split(".")[3]) <= 255:
-    print("Adreça IP:",b[:-1],a[:-1],"\nClasse A\n"+"Màscara:",binari,decimal_masc[:-1])
+    print("Adreça IP:",b,a,"\nClasse A\n"+"Màscara:",masc_binari,"=",masc,decimal_masc)
 elif 128 <= int(b.split(".")[0]) <= 191 and 0 <= int(b.split(".")[1]) <= 255 and 0 <= int(b.split(".")[2]) <= 255 and 0 <= int(b.split(".")[3]) <= 255:
-    print("Adreça IP:",b[:-1],a[:-1],"\nClasse B\n"+"Màscara:",binari,decimal_masc[:-1])
+    print("Adreça IP:",b,a,"\nClasse B\n"+"Màscara:",masc_binari,"=",masc,decimal_masc)
 elif 192 <= int(b.split(".")[0]) <= 223 and 0 <= int(b.split(".")[1]) <= 255 and 0 <= int(b.split(".")[2]) <= 255 and 0 <= int(b.split(".")[3]) <= 255:
-    print("Adreça IP:",b[:-1],a[:-1],"\nClasse C\n"+"Màscara:",binari,decimal_masc[:-1])
+    print("Adreça IP:",b,a,"\nClasse C\n"+"Màscara:",masc_binari,"=",masc,decimal_masc)
 elif 224 <= int(b.split(".")[0]) <= 239 and 0 <= int(b.split(".")[1]) <= 255 and 0 <= int(b.split(".")[2]) <= 255 and 0 <= int(b.split(".")[3]) <= 255:
-    print("Adreça IP:",b[:-1],a[:-1],"\nClasse D (Multicast)\n"+"Màscara:",binari,decimal_masc[:-1])
+    print("Adreça IP:",b,a,"\nClasse D (Multicast)\n"+"Màscara:",masc_binari,"=",masc,decimal_masc)
 elif 240 <= int(b.split(".")[0]) <= 255 and 0 <= int(b.split(".")[1]) <= 255 and 0 <= int(b.split(".")[2]) <= 255 and 0 <= int(b.split(".")[3]) <= 255:
-    print("Adreça IP:",b[:-1],a[:-1],"\nClasse E (experimental)\n"+"Màscara:",binari,decimal_masc[:-1])
+    print("Adreça IP:",b,a,"\nClasse E (experimental)\n"+"Màscara:",masc_binari,"=",masc,decimal_masc)
 else:
     print("IP incorrecta")
+    exit()
 
 count=int()
-for i, c in enumerate(b[:-1]): # "i" val la posició del element en la llista i "c" és cada caràcter de l'adreça IP per contar els punts
-    pass
-print(b)
+for i, c in enumerate(a): # "i" val la posició del element en la llista i "c" és cada caràcter de l'adreça IP per contar els punts
+    if c == '1' or c == '0':
+        count += 1
+        if count == masc:
+            a = a[:i+1]
+            a = autocompletar(a)
+            xarxa = str()
+            xarxa = conversio(xarxa,decimal,a)
+print(a,xarxa)
